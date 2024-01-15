f3a8c9f3ccef03f32ab195e72090392cc89c7d7c
Wagner de Araujo <124446545+WagDevX@users.noreply.github.com>
2024-01-15 22:25:32 +0000

    feat: component folders Table

--- src/components/foldersTable.tsx
+++ src/components/foldersTable.tsx
@@ -1,18 +1,50 @@
 
-type Props =
-    [
-        {
-            id: number,
-            name: string,
-            owner: number,
-            parentFolder: number,
-        }
-    ]
+type Props = [
+    {
+        id: number,
+        name: string,
+        owner: number,
+        exetention?: string,
+        parentFolder: number,
+    }
+]
+    
 
 
-export const FoldersTable = (Props : {}) : JSX.Element => {
+export const FoldersTable = (folders: Props) : JSX.Element => {
     return (
         <>
+        <table className="basic">
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>Tipo</th>
+                <th>Proprietário</th>
+              </tr>
+            </thead>
+            <tbody>
+                {folders.length > 0 && folders.map((folder,index) => (
+                    <>
+                    <tr key={`fol_${index}`}>
+                    <td >
+                        {folder.name}
+                    </td>
+                    <td >
+                        {folder.exetention?? "Pasta"}
+                    </td>
+                    <td >
+                        {folder.owner}
+                    </td>
+                    </tr>
+                  
+                    </>
+                )) 
+                    
+                }
+            
+            </tbody>
+          </table>
+
         </>
     )
 }
